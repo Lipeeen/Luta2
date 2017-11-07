2feff1ab1b06b8a7699472c2d8bbf46230a92c9d
ViniciusPacheco <vinicius-sp@hotmail.com>
2017-11-07 18:03:27 -0200

    atualizar

--- Luta3/Luta3/Form1.cs
+++ Luta3/Luta3/Form1.cs
@@ -12,14 +12,27 @@ namespace Luta3
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        int player;
+        public Form1(int player)
         {
             InitializeComponent();
+            this.player = player;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelTitulo.Text = string.Format("Player {0} - Escolha seu personagem", this.player);
         }
 
         private void btn_general_Click(object sender, EventArgs e)
         {
             MessageBox.Show("General!");
         }
+
+        private void btn_general_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 }
