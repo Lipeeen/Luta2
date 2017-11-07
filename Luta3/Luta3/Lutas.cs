1c21f9f6aa6f7176696dd70b5f475af08c9733c7
luizi <luizi@DESKTOP-REK0LJU>
2017-11-07 18:48:30 -0200

    lutarrrrrrr

--- Luta3/Luta3/Lutas.cs
+++ Luta3/Luta3/Lutas.cs
@@ -3,19 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Luta3.Personagens;
 
 namespace Luta3
 {
-    static class Lutas
+    class Lutas
     {
-        
-        public static void iniciar()
-        {
-            Form1 form1 = new Form1(1);
-            form1.ShowDialog();
-
-
-        }
     }
 }
